d21b11edbf2ed4a0403a062291c2195075817018
AkiraFukushima <h3.poteto@gmail.com>
2021-10-14 01:31:38 +0900

    refs #2 Create basic websocket server

---
+++ lib/hot_code_reloading_web/socket/websocket_handler.ex
@@ -0,0 +1,29 @@
+defmodule HotCodeReloadingWeb.Socket.WebSocketHandler do
+  @behaviour :cowboy_websocket
+
+  require Logger
+
+  def init(req, state) do
+    {:cowboy_websocket, req, state}
+  end
+
+  def websocket_init(state) do
+    Logger.info("started websocket connection")
+    {:ok, state}
+  end
+
+  def websocket_handle(:ping, state) do
+    {:reply, :pong, state}
+  end
+
+  def websocket_handle({:text, message}, state) do
+    {:reply, {:text, message}, state}
+  end
+
+  def websocket_info(_info, state), do: {[], state}
+
+  def terminate(_reason, _req, _state) do
+    Logger.info("terminated")
+    :ok
+  end
+end
