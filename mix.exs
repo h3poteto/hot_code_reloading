a7b4aef905d0cb406637f0db990f60cedb746bfa
AkiraFukushima <h3.poteto@gmail.com>
2021-10-17 21:49:41 +0900

    Bump to version 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HotCodeReloading.MixProject do
   def project do
     [
       app: :hot_code_reloading,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
