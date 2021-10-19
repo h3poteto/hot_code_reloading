33c7c4848a135bb2902446c85113836895af458f
AkiraFukushima <h3.poteto@gmail.com>
2021-10-20 01:53:51 +0900

    refs #17 Add code_change method for count_down

--- lib/hot_code_reloading_web/count_down/counter.ex
+++ lib/hot_code_reloading_web/count_down/counter.ex
@@ -1,5 +1,5 @@
 defmodule HotCodeReloadingWeb.CountDown.Counter do
-  @vsn "1"
+  @vsn "2"
 
   use GenServer
   require Logger
@@ -26,5 +26,15 @@ defmodule HotCodeReloadingWeb.CountDown.Counter do
   def terminate(reason, _state) do
     Logger.debug("CountDown gen_server is terminated because #{reason}")
     reason
+  end
+
+  ## for hot code reloading
+  # This method will not be called when hot code reloading, because this module
+  # is not a member of the application supervisor. This gen_server is not managed by
+  # application supervisor, this gen_server is managed by websocket handler.
+  def code_change("1" = vsn, state, _extra) do
+    Logger.info("Starting code change from #{__MODULE__} from #{vsn}")
+    %{count: count} = state
+    {:ok, %{count: count + 10_000}}
   end
 end
